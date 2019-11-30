a184a087dfff0b1e292995e9acff2d0643103313
maram-abbas <57670895+maram-abbas@users.noreply.github.com>
2019-11-30 15:48:54 +0200

    30/11/2019
    Does not yet support specialnets

--- DEF_PARSER/def_parser.py
+++ DEF_PARSER/def_parser.py
@@ -13,6 +13,7 @@ start = 100000000
 end = 0
 stop = 0
 
+#GETTING PINS
 file = open("cpu.def", "r")
 for i, line in enumerate(file):
     if line.find("PINS") != -1:
@@ -42,5 +43,177 @@ for i, line in enumerate(file):
         pin_placement_y.append(placed_y)
         
 file.close()
-            
+   
+
+#GETTING COMPONENTS  
+z = 0  
+start = 100000000
+components_name = []
+components_cell = []
+comp_n = ""
+comp_c = ""
+file = open("cpu.def", "r")
+for i, line in enumerate(file):
+    if line.find("COMPONENTS") != -1:
+        start = i
         
+    if line.find("PINS") != -1:
+        break
+        
+    if line.startswith("-") and i > start:
+        comp_n = line[2:line.find(" ",2)]
+        comp_c = line[2 + len(comp_n):line.find(" +")]
+        components_name.append(comp_n)
+        components_cell.append(comp_c)
+        #print(components_cell)
+    
+file.close()
+
+#GETTING NETS
+start = 100000000
+net_cell_instance = {}
+net = ""
+cell_name = ""
+instance = ""
+start_again = 0
+nothing = 0
+counter = 0
+metal = {}
+file = open("cpu.def", "r")
+for i, line in enumerate(file):
+    if line.find("NETS") != -1:
+        start = i
+        
+    if line.find("END NETS") != -1:
+        break
+        
+    if line.startswith("-") and i > start:
+        start_again = 1
+        counter += 1
+        net = line[2:-1]
+        net_cell_instance[net] = {}
+        net_cell_instance[net]["cell_name"] = []
+        net_cell_instance[net]["instance"] = []
+        metal[net] = {}
+        #net_cell_instance["pin" + str(counter)]["pin"] = net
+        #print(net_cell_instance)
+       
+    if start_again == 1:
+        
+        if line.startswith("  ( PIN"):
+            #do nothing
+            nothing += nothing
+            
+        elif line.startswith("  ( "):
+            cell_name = line[4:line.find(" ",4)]
+            instance = line[5 + len(cell_name):line.find(" )")]
+            net_cell_instance[net]["cell_name"].append(cell_name)
+            net_cell_instance[net]["instance"].append(instance)
+            
+        elif line.startswith("+ ROUTED"):
+            z += 1
+            routed = "+ ROUTED "
+            metal_name = line[len(routed):line.find(" (")] + "_" + str(z)
+            
+            index = line.find(" ( ") + 3
+            x1 = line[index:line.find(" ",index)]
+            
+            y1 = line[line.find(x1) + len(x1):line.find(" )")]
+            #print(y1)
+            
+            metal[net][metal_name] = {}
+            metal[net][metal_name]["x1"] = x1
+            metal[net][metal_name]["y1"] = y1[1:]
+            metal[net][metal_name]["other_x"] = []
+            metal[net][metal_name]["other_y"] = []
+            metal[net][metal_name]["merge"] = ""
+            
+            
+            for j,charac in enumerate(line):
+                
+                if charac == "(" and j != 16:   #not first bracket
+                    
+                    if line[j + 2] == '*':
+                        metal[net][metal_name]["other_x"].append(x1)
+                        
+                        if line[j + 4] == '*':
+                            metal[net][metal_name]["other_y"].append(y1[1:])
+                        else:
+                            number = line[j + 4:line.find(" )",j + 4)]
+                            metal[net][metal_name]["other_y"].append(number)
+                    else:
+                        number = line[j + 2:line.find(" ",j + 2)]
+                        metal[net][metal_name]["other_x"].append(number)
+                        
+                        if line[j + len(number) + 3] == '*':
+                            metal[net][metal_name]["other_y"].append(y1[1:])
+                        else:
+                            number = line[j + len(number) + 3:line.find(" )",j + len(number) + 3)]
+                            metal[net][metal_name]["other_y"].append(number)
+                            
+                if charac == "M":
+                    #print(line[j:-2])
+                    metal[net][metal_name]["merge"] = line[j:-2]
+                    break
+            
+            
+        elif line.startswith("  NEW metal"):
+            z += 1
+            new_metal = "  NEW "
+            metal_name = line[len(new_metal):line.find(" (")] + "_" + str(z)
+            
+            index = line.find(" ( ") + 3
+            x1 = line[index:line.find(" ",index)]
+            
+            y1 = line[line.find(x1) + len(x1) + 1:line.find(" )")]
+            #print(x1)
+            
+            metal[net][metal_name] = {}
+            metal[net][metal_name]["x1"] = x1
+            metal[net][metal_name]["y1"] = y1
+            metal[net][metal_name]["other_x"] = []
+            metal[net][metal_name]["other_y"] = []
+            metal[net][metal_name]["merge"] = ""
+            
+            
+            for j,charac in enumerate(line):
+                
+                if charac == "(" and j != 13:   #not first bracket
+                    
+                    if line[j + 2] == '*':
+                        metal[net][metal_name]["other_x"].append(x1)
+                        
+                        if line[j + 4] == '*':
+                            metal[net][metal_name]["other_y"].append(y1)
+                        else:
+                            number = line[j + 4:line.find(" )",j + 4)]
+                            metal[net][metal_name]["other_y"].append(number)
+                    else:
+                        number = line[j + 2:line.find(" ",j + 2)]
+                        metal[net][metal_name]["other_x"].append(number)
+                        
+                        if line[j + len(number) + 3] == '*':
+                            metal[net][metal_name]["other_y"].append(y1)
+                        else:
+                            number = line[j + len(number) + 3:line.find(" )",j + len(number) + 3)]
+                            metal[net][metal_name]["other_y"].append(number)
+                            
+                if charac == "M":
+                    #print(line[j:-2])
+                    if line.find(";"):
+                        metal[net][metal_name]["merge"] = line[j:-3]
+                    else:
+                        metal[net][metal_name]["merge"] = line[j:-2]
+                        
+                    break
+    
+    if line.find(";") != -1:
+        start_again = 0
+        z = 0
+      
+    #print(net_cell_instance)
+            
+                      
+file.close()
+
+print(metal["RDY"])
